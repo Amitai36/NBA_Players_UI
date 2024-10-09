4249e0fb64d3c530d17e499d132a5bf1d7e297ce
amitai <amitiy36@gmail.com>
2024-10-10 01:42:00 +0300

    craete nav bar

---
+++ client/src/layout/NavBar.tsx
@@ -0,0 +1,13 @@
+import { AppBar, Toolbar, Typography } from "@mui/material"
+
+function NavBar() {
+    return (
+        <AppBar>
+            <Toolbar>
+                <Typography variant="h4">NBA Players</Typography>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default NavBar

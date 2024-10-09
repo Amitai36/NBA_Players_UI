bd49216e4920f3b982bf5c71f9761c44a102b85b
amitai <amitiy36@gmail.com>
2024-10-10 02:38:03 +0300

    types

--- client/src/api/players.ts
+++ client/src/api/players.ts
@@ -1,6 +1,8 @@
 import axios from "axios"
 
+import { AllPlayersTypes } from "../types/players"
+
 export const getAllPlayers = async () => {
-    const res = await axios.get("api/players/getAll")
+    const res = await axios.get<AllPlayersTypes>("api/players/getAll")
     return res.data
 }
---
+++ client/src/types/players.ts
@@ -0,0 +1,33 @@
+export interface Player {
+    college: string;
+    country: string;
+    draft_number: number;
+    draft_round: number;
+    draft_year: number;
+    first_name: string;
+    height: string;
+    id: number;
+    jersey_number: string;
+    last_name: string;
+    position: string;
+    team: Team;
+    weight: string;
+}
+
+export interface Team {
+    abbreviation: string;
+    city: string;
+    conference: string;
+    division: string;
+    full_name: string;
+    id: number;
+    name: string;
+}
+
+export interface AllPlayersTypes {
+    data: Player[],
+    meta: {
+        next_cursor: number;
+        per_page: number;
+    }
+}
